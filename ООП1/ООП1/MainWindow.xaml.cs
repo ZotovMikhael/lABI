206aad287cd6ba3b3b839152f3c4843c3ec30087
ZotovMikhael <zotov.mp.rus@gmail.com>
2025-09-25 04:04:30 +0300

    Add files via upload

---
+++ ООП1/ООП1/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ООП1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void btnCreatePerson_Click(object sender, RoutedEventArgs e)
+        {
+            Person person = new Person("Ben", 18, "ben@gmail.com");
+
+            txtName.Text = person.name;
+            txtAge.Text = person.age.ToString();
+            txtEmail.Text = person.email;
+        }
+        class Person
+        {
+            public string name = "Ben";
+            public int age = 18;
+            public string email = "ben@gmail.com";
+
+            public Person(string name)
+            {
+                this.name = name;
+            }
+
+            public Person(string name, int age) : this(name)
+            {
+                this.age = age;
+            }
+
+            public Person(string name, int age, string email) : this("Bob", age)
+            {
+                this.email = email;
+            }
+        
+        }
+    }
+}
